099345206371dce38d478b4315ef7fd2c2c2944e
rabidpraxis <webster.kevin@gmail.com>
2012-09-01 16:15:48 -0700

    removed ssl code

--- lib/carbon-copy/cache_server.rb
+++ lib/carbon-copy/cache_server.rb
@@ -16,53 +16,6 @@ module CarbonCopy
       end
     end
 
-    def ssh_run(port)
-      begin
-        server = TCPServer.new(port)
-        cert, priv = create_cert
-        context = OpenSSL::SSL::SSLContext.new
-        context.cert = OpenSSL::X509::Certificate.new(cert)
-        context.key = OpenSSL::PKey::RSA.new(priv)
-        ssl_server = OpenSSL::SSL::SSLServer.new(server, context)
-
-        while (session = ssl_server.accept)
-          Thread.new(session, &method(:handle))
-        end
-      rescue => e
-        p e.message
-        p e.backtrace
-      end
-    end
-
-    def create_cert
-      key = OpenSSL::PKey::RSA.new(1024)
-      public_key = key.public_key
-
-      subject = "/C=BE/O=Test/OU=Test/CN=Test"
-
-      cert = OpenSSL::X509::Certificate.new
-      cert.subject = cert.issuer = OpenSSL::X509::Name.parse(subject)
-      cert.not_before = Time.now
-      cert.not_after = Time.now + 365 * 24 * 60 * 60
-      cert.public_key = public_key
-      cert.serial = 0x0
-      cert.version = 2
-
-      ef = OpenSSL::X509::ExtensionFactory.new
-      ef.subject_certificate = cert
-      ef.issuer_certificate = cert
-      cert.extensions = [
-        ef.create_extension("basicConstraints","CA:TRUE", true),
-        ef.create_extension("subjectKeyIdentifier", "hash"),
-      ]
-      cert.add_extension ef.create_extension("authorityKeyIdentifier",
-                                             "keyid:always,issuer:always")
-
-      cert.sign key, OpenSSL::Digest::SHA1.new
-
-      [cert.to_pem, key]
-    end
-
     def parse_request(session)
       request = session.readline
 
@@ -103,9 +56,6 @@ module CarbonCopy
         end
       end
       header
-    end
-
-    def ssl_handle(session)
     end
 
     def handle(session)
